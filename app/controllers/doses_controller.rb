2d127a5e84dde496279185c6fd10cf3a0b54d55e
Rodrigo Birolli <rodrigo.birolli@gmail.com>
2016-11-24 15:40:55 -0200

    Implentent new doses, pending delete doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,30 +1,28 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail, only: [ :new, :create, :destroy ]
+  before_action :find_cocktail, only: [ :new, :create ]
 
   def new
-    puts ">>>>>>>>>>>>>>>>>>>   new   <<<<<<<<<<<<<<<<<<<<<<<<<"
     @dose = Dose.new
   end
 
   def create
-    puts ">>>>>>>>>>>>>>>>>>>   create   <<<<<<<<<<<<<<<<<<<<<<<<<"
     @dose = @cocktail.doses.build(dose_params)
     @dose.save
     redirect_to cocktail_path(@cocktail)
   end
 
-  def destroy
-    @dose.destroy
+  def destroy(dose)
+    dose = Dose.find(params[:id])
+    dose.destroy
     respond_to do |format|
       format.html { redirect_to cocktails_url, notice: 'Doses was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :name)
+    params.require(:dose).permit(:description, :name, :cocktail_id, :ingredient_id)
   end
 
   def find_cocktail
